c89a981f7cabe30a73231fef12f25da0f5870f71
Cynthia Kitili <nyambucindy0@gmail.com>
2020-12-05 16:00:07 +0300

    add initialisation function of user class

---
+++ app/models.py
@@ -0,0 +1,8 @@
+class User:
+    '''
+    User class to define user objects
+    '''
+
+    def __init__(self, username,password):
+        self.username = username
+        self.password = password
