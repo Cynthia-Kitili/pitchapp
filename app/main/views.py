4bc56d3d0de349e02c27aaf87edf8529e883a3d5
Cynthia Kitili <nyambucindy0@gmail.com>
2020-12-06 20:42:25 +0300

    add search option

--- app/main/views.py
+++ app/main/views.py
@@ -53,3 +53,36 @@ def promotion():
     pitches= Pitch.get_all_pitches()
 
     return render_template('promotion.html', title = title, pitches= pitches )
+
+@main.route('/product/pitches/')
+def product():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Product Pitches'
+    pitches= Pitch.get_all_pitches()
+    return render_template('product.html', title = title, pitches= pitches )
+ 
+#  end of category root functions
+
+@main.route('/pitch/<int:pitch_id>')
+def pitch(pitch_id):
+
+    '''
+    View pitch page function that returns the pitch details page and its data
+    '''
+    found_pitch= get_pitch(pitch_id)
+    title = pitch_id
+    pitch_comments = Comment.get_comments(pitch_id)
+
+    return render_template('pitch.html',title= title ,found_pitch= found_pitch, pitch_comments= pitch_comments)
+
+@main.route('/search/<pitch_name>')
+def search(pitch_name):
+    '''
+    View function to display the search results
+    '''
+    searched_pitches = search_pitch(pitch_name)
+    title = f'search results for {pitch_name}'
+
+    return render_template('search.html',pitches = searched_pitches)
