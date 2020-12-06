99935e32da9c30291077e5d76923d51b01780614
Cynthia Kitili <nyambucindy0@gmail.com>
2020-12-06 20:38:34 +0300

    add pitchform option

--- app/main/forms.py
+++ app/main/forms.py
@@ -10,3 +10,8 @@ class CommentsForm(FlaskForm):
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')     
+
+class PitchForm(FlaskForm):
+    category_id = SelectField('Select Category', choices=[('1', 'Interview'), ('2', 'Pick Up Lines'), ('3', 'Promotion'),('4','Product')])
+    content = TextAreaField('YOUR PITCH')
+    submit = SubmitField('Create Pitch')    
