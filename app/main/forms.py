6eaf423c0d08e912ac6bb219fbfc4efd07d34ca2
Cynthia Kitili <nyambucindy0@gmail.com>
2020-12-06 20:39:06 +0300

    add upvote in form

--- app/main/forms.py
+++ app/main/forms.py
@@ -15,3 +15,9 @@ class PitchForm(FlaskForm):
     category_id = SelectField('Select Category', choices=[('1', 'Interview'), ('2', 'Pick Up Lines'), ('3', 'Promotion'),('4','Product')])
     content = TextAreaField('YOUR PITCH')
     submit = SubmitField('Create Pitch')    
+
+class UpvoteForm(FlaskForm):
+    '''
+    Class to create a wtf form for upvoting a pitch
+    '''
+    submit = SubmitField('Upvote')    
