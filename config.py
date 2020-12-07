82bba7c62db05f608fd64314a5048557455c7bed
Cynthia Kitili <nyambucindy0@gmail.com>
2020-12-07 12:25:59 +0300

    changes made to gitignore

--- config.py
+++ config.py
@@ -1,50 +1,26 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config:
-    '''
-    General configuration parent class
-    '''
-    SECRET_KEY = 'utility'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cynthia:gitz254@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY=os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    
-    #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587 
-    MAIL_USE_TLS = True
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'Pitch'
-    SENDER_EMAIL = 'nyambucynthia98@gmail.com'
-
-    # simple mde  configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
-
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vincent:Empharse333@localhost/pitch_test'
-
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    
-class DevConfig(Config):
-    '''
-    Development  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    
 
+class DevConfig(Config):
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig,
-'test':TestConfig
-}    
+'production':ProdConfig
+}
--- manage.py
+++ manage.py
@@ -1,23 +1,26 @@
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
-from flask_script import Manager,Server
-from app.models import User, Role, Comment,Pitch
-from  flask_migrate import Migrate, MigrateCommand
+from app.models import User
 
-#creating an app instance  
-app = create_app('production')  
-
+app = create_app('production')
 
 manager = Manager(app)
-manager.add_command('server',Server)
-
-manager.add_command('server',Server)
-
 migrate = Migrate(app,db)
+
 manager.add_command('db',MigrateCommand)
+manager.add_command('run',Server(use_debugger=True))
+
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Pitch = Pitch, Role = Role, Comment = Comment ) 
+    return dict(app = app,db = db,User = User)
 
-if __name__ == '__main__':
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == "__main__":
     manager.run()
