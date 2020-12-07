629452559b8c4e21d9c3ec0c8158206098e0d158
Cynthia Kitili <nyambucindy0@gmail.com>
2020-12-07 11:35:22 +0300

    add secret key

--- config.py
+++ config.py
@@ -1,27 +1,22 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
-    pass
+    SECRET_KEY = 'TheDifference'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cynthia:gitz254@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587 
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Pitch'
+    SENDER_EMAIL = 'nyambucynthia98@gmail.com'
 
-
-
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
-
-
-class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
-    DEBUG = True
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
