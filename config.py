514450cabb1b05e933f055a522a3cf32a1a32978
Cynthia Kitili <nyambucindy0@gmail.com>
2020-12-08 18:59:28 +0300

    add category pitches

--- config.py
+++ config.py
@@ -3,24 +3,36 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS=True
-    SECRET_KEY=os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cynthia:gitz254@localhost/pitches'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SECRET_KEY='cynde'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    # MAIL_USE_SSL = True
+    MAIL_USERNAME = 'nyambucindy0@gmail.com'
+    MAIL_PASSWORD = 'rem1436tuit10'
+    # SENDER_EMAIL = 'nyambucindy0@gmail.com'
+
+
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cynthia:gitz254@localhost/pitches_test'
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cynthia:gitz254@localhost/pitches_test'    
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cynthia:gitz254@localhost/pitches'
+    
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }
---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='cynde'
+export MAIL_USERNAME='nyambucindy0@gmail.com'
+export MAIL_PASSWORD='rem1436tuit10'
+
+python3.9 manage.py server
